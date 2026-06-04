ddcf853a054598e0c154c283281f3cdc14538130
Haseeb Ahmad <haseebahmaduet4@gmail.com>
2026-06-03 20:24:38 -0700

    Complaint added in models

---
+++ models/Complaint.hpp
@@ -0,0 +1,23 @@
+#ifndef COMPLAINT_HPP
+#define COMPLAINT_HPP
+
+#include<string>
+using namespace std;
+
+class ComplaintNode
+{
+    int complaintID;
+    int stuRollNo;
+    string desc;
+    ComplaintNode * next;
+
+    ComplaintNode(int id, int rollNo, string desc)
+    {
+        complaintID = id;
+        stuRollNo = rollNo;
+        desc = desc;
+        next = nullptr;
+    }
+};
+
+#endif
