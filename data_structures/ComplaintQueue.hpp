4aea11a7a03f8dc9930bfa3efa5ebccbed638faa
Haseeb Ahmad <haseebahmaduet4@gmail.com>
2026-06-03 20:26:16 -0700

    ComplaintQueue added in data_structures

---
+++ data_structures/ComplaintQueue.hpp
@@ -0,0 +1,58 @@
+#ifndef COMPLAINTQUEUE_HPP
+#define COMPLAINTQUEUE_HPP
+
+#include"../models/Complaint.hpp"
+#include<iostream>
+using namespace std;
+
+class ComplaintQueue
+{
+    private:
+    ComplaintNode * front;
+    ComplaintNode * rear;
+
+    public:
+    ComplaintQueue() : front(nullptr), rear(nullptr) {}
+
+    ~ComplaintQueue() // to free all dynamically allocated nodes
+    {
+        while(!isEmpty())
+        {
+            Dequeue();
+        }
+    }
+
+    void Enqueue(int id, int rollNo, string desc)
+    {
+        ComplaintNode * newNode = new ComplaintNode(id, rollNo, desc);
+        if(isEmpty())
+        {
+            front =  rear = newNode;
+            return;
+        }
+        rear->next = newNode;
+        rear = newNode;
+    }
+
+    void Dequeue()
+    {
+        if(isEmpty()) return false;
+
+        ComplaintNode * temp = front;
+        front = front->next;
+
+        if(front == nullptr)
+        {
+            rear = nullptr;
+        }
+        delete temp;
+        return true;
+    }
+
+    ComplaintNode * getFront()
+    {
+        return front;
+    }
+};
+
+#endif
