41fa3e746e7e7fe81c08611742f3e56004a661d4
Haseeb Ahmad <haseebahmaduet4@gmail.com>
2026-06-10 07:48:52 -0700

    undo/redo added for students

--- modules/StudentManager.hpp
+++ modules/StudentManager.hpp
@@ -1,14 +1,27 @@
 #pragma once
 #include"../data_structures/DynamicArray.hpp"
+#include"../data_structures/GenericStack.hpp"
 #include"../utils/FileManager.hpp"
 #include"../models/Student.hpp"
 using namespace std;
 
+// unique payload struct that will be used as snapshot state
+struct StudentAction
+{
+    string type;
+    int rollNo;
+    string name;
+    string department;
+    float cgpa;
+};
 class StudentManager
 {
     private:
     DynamicArray<Student> studentRecords;
     const string fileName = "D:/Smart University System/students.txt";
+
+    GenericStack<StudentAction> undoStack;
+    GenericStack<StudentAction> redoStack;
 
     public:
     void InitSystem()
@@ -20,9 +33,77 @@ class StudentManager
     {
         cout << "Adding student: " << rollNo << " - " << name << "\n";
         Student s = {rollNo,name,dept,cgpa};
-        cout << "Student object created\n";
         studentRecords.push_back(s);
-        cout << "Student added to records\n";
+        cout << "Student successfully added to records\n";
+
+        // Log a snapshot payload onto the undo history tracker
+        StudentAction action = {"ADD",rollNo, name, dept, cgpa};
+        undoStack.push(action);
+
+        redoStack.clear();
+    }
+
+    void executeUndo()
+    {
+        if(undoStack.isEmpty())
+        {
+            cout<<"[Undo System] Alert: No student record left to revert\n";
+            return;
+        }
+
+        StudentAction lastAction = undoStack.pop();
+        // pushing it in redoStack, if the user wants to do redo after undo
+        redoStack.push(lastAction);
+
+        if(lastAction.type == "ADD") // this ADD ensures whether the student was added or not
+        {
+            // Delete from memory (studentRecords array)
+            int targetIndex = -1;
+            for(int i = 0; i < studentRecords.getSize(); i++)
+            {
+                if(studentRecords.get(i).rollNo == lastAction.rollNo)
+                {
+                    targetIndex = i;
+                    break;
+                }
+            }
+            
+            if(targetIndex != -1)
+            {
+                studentRecords.removeAt(targetIndex);
+            }
+
+            // Delete from file
+            FileManager::DeleteStudentFromFile(fileName, lastAction.rollNo);
+
+            cout << "[Undo Action] Successfully deleted mistakenly added Student Roll No: " << lastAction.rollNo << "\n";
+        }
+    }
+
+    // redoing the previously undone step
+    void executeRedo()
+    {
+        if(redoStack.isEmpty())
+        {
+            cout<<"[Redo Alert]: Already at the latest action state\n";
+            return;
+        }
+
+        StudentAction actionToRedo = redoStack.pop();
+        // push it back on the active undo stack
+        undoStack.push(actionToRedo);
+
+        if(actionToRedo.type == "ADD")
+        {
+            // Re-add the student to memory
+            Student s(actionToRedo.rollNo, actionToRedo.name, actionToRedo.department, actionToRedo.cgpa);
+            studentRecords.push_back(s);
+
+            // Re-add to file by saving the entire state
+            FileManager::SaveStudents(fileName, studentRecords);
+
+            cout << "[Redo Action] Re-inserted Student Roll No: " << actionToRedo.rollNo << " back into the database.\n";
+        }
     }
 
     void SaveSystemState()
