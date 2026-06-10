7fbfc6ef2235668f22cd09a27a5b19fc4a83eb19
Haseeb Ahmad <haseebahmaduet4@gmail.com>
2026-06-10 07:48:03 -0700

    undo/redo added for complaints

--- modules/ComplaintManager.hpp
+++ modules/ComplaintManager.hpp
@@ -3,9 +3,18 @@
 
 #include"../data_structures/ComplaintQueue.hpp"
 #include"../data_structures/TokenCircularQueue.hpp"
+#include"../data_structures/GenericStack.hpp"
 #include<iostream>
 #include<string>
 using namespace std;
+
+struct ComplaintAction
+{
+    string type;
+    int complaintID;
+    int stuRollNo;
+    string desc;
+};
 
 class ComplaintManager
 {
@@ -15,6 +24,9 @@ class ComplaintManager
 
     int complaintID;
     int tokenNumber;
+
+    GenericStack<ComplaintAction> undoStack;
+    GenericStack<ComplaintAction> redoStack;
 
     public:
     ComplaintManager()
@@ -27,9 +39,16 @@ class ComplaintManager
     
     void FileComplaint(int rollNo, string desc)
     {
+        // Store action with CURRENT ID before incrementing
+        ComplaintAction action = {"ADD", complaintID, rollNo, desc};
+        
         complaints.Enqueue(complaintID, rollNo, desc);
         cout<<"Complaint registered successfully! Complaint ID: " <<complaintID <<"\n";
-        complaintID++;
+        
+        undoStack.push(action);
+        redoStack.clear();
+        
+        complaintID++;  // Increment AFTER storing in action
     }
 
     void resolveComplaint()
@@ -43,6 +62,52 @@ class ComplaintManager
         complaints.Dequeue();
         cout<<"Status: Resolved and Dropped from queue successfully! \n";
 
+    }
+
+    void executeUndo()
+    {
+        if(undoStack.isEmpty())
+        {
+            cout<<"[Undo System] Alert: No complaints left to revert.\n";
+            return;
+        }
+
+        ComplaintAction lastAction = undoStack.pop();
+        redoStack.push(lastAction);
+
+        if(lastAction.type == "ADD")
+        {
+            // Remove from REAR (newest/last added complaint) - for undo
+            complaints.DequeueFromRear();
+            cout<<"[Undo Action]: Removed newest Complaint ID " <<lastAction.complaintID <<" from queue\n";
+            
+            complaintID--;  // Decrement the ID counter
+        }
+    }
+
+    void executeRedo()
+    {
+        if(redoStack.isEmpty())
+        {
+            cout<<"[Redo System] Alert: Already at the latest action state\n";
+            return;
+        }
+
+        ComplaintAction actionToRedo = redoStack.pop();
+        undoStack.push(actionToRedo);
+
+        if (actionToRedo.type == "ADD") {
+            // Re-add complaint to queue with original ID
+            complaints.Enqueue(actionToRedo.complaintID, actionToRedo.stuRollNo, actionToRedo.desc);
+            cout << "[Redo Action] Re-inserted Complaint ID " << actionToRedo.complaintID << " back into the queue.\n";
+            
+            complaintID++;  
+        }
+    }
+
+    void viewActiceComplaints()
+    {
+        complaints.Display();
     }
 
     // TOKENS
